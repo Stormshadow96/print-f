409be39e2634606a54f073bea3be49bb29195f5e
Uncool300 <uncoolyemi@gmail.com>
2023-09-21 16:40:39 -0700

    utilities.c

---
+++ ut.c
@@ -0,0 +1,75 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * LOMAx
+ * ut.c  - Caculates the utilities to cast the argument
+ * @format: Formatted string in which to print the arguments
+ * @i: List of srgumnts to be printed
+ *
+ * Return: Precision
+ */
+int ut.c(const char *format, int *i)
+{
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code is always 2 digits long */
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	bufffer[i] = map_to[ascii_code % 16];
+
+	return (3);
+}
+
+/**
+ * is_digit - This verifies if a char is a digit
+ * @c: Char to be evaluated
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+
+/**
+ * convert_size_number - Casts a number to the specified size
+ * @num: Number to be casted
+ * @size: Number indicating the type to be casted
+ * LOMAx
+ * Return: Casted value of num
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
+}
+
+/**
+ * convert_size_unsgnd - Casts a number to the specified size
+ * @num: Number to be casted
+ * @size: Number indicating the type to be casted
+ * LOMAX - Young
+ * Return: Casted value of the num
+ */
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
+}
