e01fd14dbd1a0fcb6516d4ce0ec81d110498b158
stormshadow96 <jletsoalo257@gmail.com>
2023-09-21 17:32:18 -0700

    done

--- ut.c
+++ ut.c
@@ -1,16 +1,27 @@
-#include <limits.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
- * LOMAx
- * ut.c  - Caculates the utilities to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of srgumnts to be printed
+ * is_printable - Evaluates if a char is printable
+ * @c: Char to be evaluated.
  *
- * Return: Precision
+ * Return: 1 if c is printable, 0 otherwise
  */
-int ut.c(const char *format, int *i)
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * @buffer: Array of chars.
+ * @i: Index at which to start appending.
+ * @ascii_code: ASSCI CODE.
+ * Return: Always 3
+ */
+int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
 	/* The hexa format code is always 2 digits long */
@@ -21,13 +32,13 @@ int ut.c(const char *format, int *i)
 	buffer[i++] = 'x';
 
 	buffer[i++] = map_to[ascii_code / 16];
-	bufffer[i] = map_to[ascii_code % 16];
+	buffer[i] = map_to[ascii_code % 16];
 
 	return (3);
 }
 
 /**
- * is_digit - This verifies if a char is a digit
+ * is_digit - Verifies if a char is a digit
  * @c: Char to be evaluated
  *
  * Return: 1 if c is a digit, 0 otherwise
@@ -42,9 +53,9 @@ int is_digit(char c)
 
 /**
  * convert_size_number - Casts a number to the specified size
- * @num: Number to be casted
- * @size: Number indicating the type to be casted
- * LOMAx
+ * @num: Number to be casted.
+ * @size: Number indicating the type to be casted.
+ *
  * Return: Casted value of num
  */
 long int convert_size_number(long int num, int size)
@@ -61,8 +72,8 @@ long int convert_size_number(long int num, int size)
  * convert_size_unsgnd - Casts a number to the specified size
  * @num: Number to be casted
  * @size: Number indicating the type to be casted
- * LOMAX - Young
- * Return: Casted value of the num
+ *
+ * Return: Casted value of num
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
